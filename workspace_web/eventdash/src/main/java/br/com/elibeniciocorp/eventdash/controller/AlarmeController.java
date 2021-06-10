933d67d0b301095633218421cd02fa3cfa9c62dd
elibenicio <84859767+elibenicio@users.noreply.github.com>
2021-06-10 14:48:05 -0300

    Add files via upload

---
+++ workspace_web/eventdash/src/main/java/br/com/elibeniciocorp/eventdash/controller/AlarmeController.java
@@ -0,0 +1,29 @@
+package br.com.elibeniciocorp.eventdash.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.elibeniciocorp.eventdash.dao.AlarmeDAO;
+import br.com.elibeniciocorp.eventdash.model.Alarme;
+
+
+@RestController
+@CrossOrigin("*")
+
+public class AlarmeController {
+	
+	@Autowired
+	private AlarmeDAO dao;
+	
+	@GetMapping("/alarmes")
+	public ArrayList<Alarme> recuperarTodos(){
+		ArrayList<Alarme> lista;
+		lista = (ArrayList<Alarme>)dao.findAll();
+		return lista;
+	}
+
+}
